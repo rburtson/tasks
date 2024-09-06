78a59217117571ff0073b0baa69a22c31821bd67
Ryan <rburtson@udel.edu>
2024-09-06 00:01:35 -0400

    Added HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,65 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
+            <header className="App-header" style={{ color: "black" }}>
                 UD CISC275 with React Hooks and TypeScript and RYAN JOSEPH
                 BURTSON
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Additionally, Hello World...
-            </p>
-            <Button>Buttoff</Button>
-            <h1>Hi :3</h1>
-            <img src=
+            <Container>
+                <Row>
+                    <Col>
+                        <p>
+                            Edit <code>src/App.tsx</code> and save. This page
+                            will automatically reload. Additionally, Hello
+                            World...
+                        </p>
+                        <Button
+                            onClick={() => {
+                                console.log("Hello World!");
+                            }}
+                        >
+                            Log Hello World
+                        </Button>
+                        <div></div>
+                        <h1>Hi :3</h1>
+                        <img
+                            src="src\Images\smiggie.jpg"
+                            alt="A picture of Biggie Smalls"
+                        />
+                        <div
+                            style={{
+                                backgroundColor: "red",
+                                height: "100px",
+                                width: "500px",
+                                margin: "auto",
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <p>Things I like:</p>
+                        <p>
+                            <ol id="OrderedList">
+                                <li>Black Cats</li>
+                                <li>Orange Cats</li>
+                                <li>Tuxedo Cats</li>
+                                <li>Not Much Else</li>
+                            </ol>
+                        </p>
+                        <div
+                            style={{
+                                backgroundColor: "red",
+                                height: "100px",
+                                width: "500px",
+                                margin: "auto",
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
