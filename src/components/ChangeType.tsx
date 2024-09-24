291df7f8de7960b042198f8fd4d7965602679a7c
Ryan <rburtson@udel.edu>
2024-09-23 20:13:12 -0400

    Solved State Functions

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,22 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [qType, setQType] = useState<QuestionType>("short_answer_question");
+
+    function flipQType(): void {
+        setQType(
+            qType === "short_answer_question" ?
+                "multiple_choice_question"
+            :   "short_answer_question",
+        );
+    }
+
+    return (
+        <div>
+            <Button onClick={flipQType}>Change Type</Button>
+            {qType === "short_answer_question" ?
+                <div>Short Answer</div>
+            :   <div>Multiple Choice</div>}
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -7,10 +7,36 @@ import { Button } from "react-bootstrap";
  * module (which returns a random decimal between 0 up until 1) in order
  * to produce a random integer between 1 and 6 (inclusive).
  */
-export function d6(): number {
+function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeftDie] = useState<number>(1);
+    const [rightDie, setRightDie] = useState<number>(2);
+
+    function left(): void {
+        setLeftDie(d6);
+    }
+
+    function right(): void {
+        setRightDie(d6);
+    }
+
+    return (
+        <div>
+            <Button onClick={left}>Roll Left</Button>
+            <div></div>
+            Die 1s value is: <span data-testid="left-die">{leftDie}</span>
+            <div></div>
+            <Button onClick={right}>Roll Right</Button>
+            <div></div>
+            Die 2s value is: <span data-testid="right-die">{rightDie}</span>
+            {leftDie === rightDie ?
+                leftDie === 1 ?
+                    <div>Snake Eyes! You Lose!</div>
+                :   <div>Doubles! You Win</div>
+            :   <div>Keep Rolling!</div>}
+        </div>
+    );
 }
