050bc27e231c69b668a5a9a5fa5c5cf786505b43
Ryan <rburtson@udel.edu>
2024-09-24 21:37:13 -0400

    Fixed Shove Box

--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,49 +1,42 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+interface shoveBoxParams {
+    position: number;
+    setPosition: (newPosition: number) => void;
+}
+
+function MoveableBox({ position }: shoveBoxParams): React.JSX.Element {
+    return (
+        <div
+            data-testid="moveable-box"
+            style={{
+                width: "50px",
+                height: "50px",
+                backgroundColor: "lightblue",
+                border: "1px solid blue",
+                display: "inline-block",
+                verticalAlign: "bottom",
+                marginLeft: position + "px",
+            }}
+        ></div>
+    );
+}
+
+function ShoveBoxButton({ position, setPosition }: shoveBoxParams) {
+    return (
+        <Button
+            onClick={() => {
+                setPosition(4 + position);
+            }}
+        >
+            Move Box
+        </Button>
+    );
+}
+
 export function ShoveBox(): React.JSX.Element {
     const [position, setPosition] = useState<number>(10);
-
-    function MoveableBox({
-        position,
-        setPosition,
-    }: {
-        position: number;
-        setPosition: (newPosition: number) => void;
-    }): React.JSX.Element {
-        return (
-            <div
-                data-testid="moveable-box"
-                style={{
-                    width: "50px",
-                    height: "50px",
-                    backgroundColor: "lightblue",
-                    border: "1px solid blue",
-                    display: "inline-block",
-                    verticalAlign: "bottom",
-                    marginLeft: position + "px",
-                }}
-            ></div>
-        );
-    }
-
-    function ShoveBoxButton({
-        position,
-        setPosition,
-    }: {
-        position: number;
-        setPosition: (newPosition: number) => void;
-    }) {
-        return (
-            <Button
-                onClick={() => {
-                    setPosition(4 + position);
-                }}
-            >
-                Move Box
-            </Button>
-        );
-    }
 
     return (
         <div>
